d5e79e4dfc64af6d5590d38eaafa8c3b09644be8
Dluck <dutiesheng@outlook.com>
2021-04-10 16:21:14 +0800

    Init Commit

---
+++ Assets/Code/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Code{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
