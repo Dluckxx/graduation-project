09b35bc4e4e26c8e76440734032d7dd9a59226b7
杜铁生 <dutiesheng@outlook.com>
2021-05-07 17:21:19 +0800

    Scene load

--- source/Game/Application/Main.cs
+++ source/Game/Application/Main.cs
@@ -1,16 +1,20 @@
 using Game.Service;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Game.Application{
     public static class Main
     {
         // Map prefab
         public static GameObject MainMap { get; set; }
+        public static List<string> SceneList { get; set; }
 
         public static void Setup()
         {
             ServiceContext.Setup();
             LoadMap();
+            LoadScene();
         }
 
         public static void TearDown()
@@ -32,6 +36,14 @@ namespace Game.Application{
             else
             {
                 Debug.LogError("[Application][Main] - No map founded!");
+            }
+        }
+
+        private static void LoadScene()
+        {
+            foreach(string name in SceneList)
+            {
+                SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
             }
         }
     }
--- source/Game/Application/MainComponent.cs
+++ source/Game/Application/MainComponent.cs
@@ -1,14 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Game.Application{
     public class MainComponent : MonoBehaviour
     {
         public GameObject map;
 
+        public List<string> scenes;
+
         private void Awake()
         {
             Main.MainMap = map;
-
+            Main.SceneList = scenes;
             Main.Setup();
             Debug.Log("[Application][MainComponent] - Starting game ...");
         }
